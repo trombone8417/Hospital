e23d1d314e30331ec0be79e863b396a53545dc9d
trombone8417 <trombone8417@yahoo.com>
2024-07-20 12:05:01 +0800

    Windows Form Login Page

---
+++ HMS/HMS/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HMS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            String username = txtUserName.Text;
+            String pass = txtPassword.Text;
+
+            if(username == "hms"  && pass == "pass")
+            {
+                MessageBox.Show("you have entered right username and password");
+            }
+            else
+            {
+                MessageBox.Show("wrong username and password");
+            }
+        }
+    }
+}
