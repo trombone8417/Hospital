014e2796dc2a29e60be3d7b268df4753893c02a9
trombone8417 <trombone8417@yahoo.com>
2024-07-22 08:55:40 +0800

    Insert AddPatient

--- HMS/HMS/Dashboard.Designer.cs
+++ HMS/HMS/Dashboard.Designer.cs
@@ -36,6 +36,26 @@ namespace HMS
             this.btnFullHistory = new System.Windows.Forms.Button();
             this.btnHospital = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtBlood = new System.Windows.Forms.TextBox();
+            this.txtAny = new System.Windows.Forms.TextBox();
+            this.txtPID = new System.Windows.Forms.TextBox();
+            this.comboGender = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -66,6 +86,7 @@ namespace HMS
             this.btnAddNew.TabIndex = 2;
             this.btnAddNew.Text = "Add New Patient Record";
             this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddPatient_Click);
             // 
             // btnAdd
             // 
@@ -104,11 +125,189 @@ namespace HMS
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.comboGender);
+            this.panel1.Controls.Add(this.txtPID);
+            this.panel1.Controls.Add(this.txtAny);
+            this.panel1.Controls.Add(this.txtBlood);
+            this.panel1.Controls.Add(this.txtAge);
+            this.panel1.Controls.Add(this.txtContact);
+            this.panel1.Controls.Add(this.txtAddress);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(205, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(638, 344);
+            this.panel1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Add New Patient Record";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(63, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 12);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Contact Number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(63, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 12);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Age";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(63, 172);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 12);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Gender";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(63, 197);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 12);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Blood Group";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(63, 223);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(166, 12);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Any Major Diease Suffered Earlier";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(63, 277);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(51, 12);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Patient ID";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(194, 59);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(298, 22);
+            this.txtName.TabIndex = 9;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(194, 87);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(298, 22);
+            this.txtAddress.TabIndex = 10;
+            // 
+            // txtContact
+            // 
+            this.txtContact.Location = new System.Drawing.Point(194, 115);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(298, 22);
+            this.txtContact.TabIndex = 11;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(194, 143);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(298, 22);
+            this.txtAge.TabIndex = 12;
+            // 
+            // txtBlood
+            // 
+            this.txtBlood.Location = new System.Drawing.Point(194, 194);
+            this.txtBlood.Name = "txtBlood";
+            this.txtBlood.Size = new System.Drawing.Size(298, 22);
+            this.txtBlood.TabIndex = 13;
+            // 
+            // txtAny
+            // 
+            this.txtAny.Location = new System.Drawing.Point(65, 243);
+            this.txtAny.Name = "txtAny";
+            this.txtAny.Size = new System.Drawing.Size(427, 22);
+            this.txtAny.TabIndex = 14;
+            // 
+            // txtPID
+            // 
+            this.txtPID.Location = new System.Drawing.Point(65, 292);
+            this.txtPID.Name = "txtPID";
+            this.txtPID.Size = new System.Drawing.Size(317, 22);
+            this.txtPID.TabIndex = 15;
+            // 
+            // comboGender
+            // 
+            this.comboGender.FormattingEnabled = true;
+            this.comboGender.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Other"});
+            this.comboGender.Location = new System.Drawing.Point(194, 169);
+            this.comboGender.Name = "comboGender";
+            this.comboGender.Size = new System.Drawing.Size(298, 20);
+            this.comboGender.TabIndex = 16;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(418, 286);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 17;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(885, 368);
+            this.ClientSize = new System.Drawing.Size(839, 343);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnHospital);
             this.Controls.Add(this.btnFullHistory);
@@ -119,6 +318,8 @@ namespace HMS
             this.Name = "Dashboard";
             this.Text = "Dashboard";
             this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,5 +334,24 @@ namespace HMS
         private System.Windows.Forms.Button btnFullHistory;
         private System.Windows.Forms.Button btnHospital;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox comboGender;
+        private System.Windows.Forms.TextBox txtPID;
+        private System.Windows.Forms.TextBox txtAny;
+        private System.Windows.Forms.TextBox txtBlood;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
--- HMS/HMS/Dashboard.cs
+++ HMS/HMS/Dashboard.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -24,7 +25,55 @@ namespace HMS
 
         private void Dashboard_Load(object sender, EventArgs e)
         {
+            panel1.Visible = false;
+        }
 
+        private void btnAddPatient_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String name = txtName.Text;
+                String address = txtAddress.Text;
+                Int64 contact = Convert.ToInt64(txtContact.Text);
+                int age = Convert.ToInt32(txtAge.Text);
+                String gender = comboGender.Text;
+                String blood = txtBlood.Text;
+                String any = txtAny.Text;
+                int pid = Convert.ToInt32(txtPID.Text);
+
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "data source=DESKTOP-E4UN3CO;initial catalog=hospital;integrated security=true;";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+
+                cmd.CommandText = "insert into AddPatient values ('" + name + "','" + address + "'," + contact + "," + age + ",'" + gender + "','"+ blood + "','" + any + "'," + pid + ");";
+
+                SqlDataAdapter DA = new SqlDataAdapter(cmd);
+                DataSet DS = new DataSet();
+
+                DA.Fill(DS);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("indvalid data format or invalid pid");
+            }
+
+            MessageBox.Show("Data Saved!");
+
+            txtName.Clear();
+            txtAddress.Clear();
+            txtContact.Clear();
+            txtAge.Clear();
+            txtBlood.Clear();
+            txtAny.Clear();
+            txtPID.Clear();
+            comboGender.ResetText();
         }
     }
 }
